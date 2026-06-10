9dc35371a459890b1eda9d7b88af055b1a68ea33
leticiamaciel0 <leticia.maciel09@aluno.ifce.edu.br>
2026-06-10 15:40:49 -0300

    feat: criar view servicos.blade.php
    Implementação da interface para a rota '/servicos'. O ficheiro foi estruturado para ser renderizado através do método servicos() no PaginaController, consolidando a integração entre Controller e View.

---
+++ laravel-app/resources/views/servicos.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Nossos Serviços</title>
+</head>
+<body>
+    <h1>Nossos Serviços</h1>
+    <ul>
+        <li>Desenvolvimento de Software</li>
+        <li>Consultoria em TI</li>
+        <li>Análise de Dados</li>
+    </ul>
+</body>
+</html>
