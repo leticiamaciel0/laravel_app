a3c5cbb37d92b5e87ce3d5629bacb32fa136418d
leticiamaciel0 <leticia.maciel09@aluno.ifce.edu.br>
2026-06-10 15:45:31 -0300

    fix: criar view equipe.blade.php
    Implementação da interface para a rota '/equipe'. Este commit, em conjunto com a criação do método no Controller, soluciona o erro de execução planejado na atividade prática.

---
+++ laravel-app/resources/views/equipe.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Nossa Equipe</title>
+</head>
+<body>
+    <h1>Conheça Nossa Equipe</h1>
+    <p>Trabalhamos com os melhores profissionais do IFCE!</p>
+</body>
+</html>
